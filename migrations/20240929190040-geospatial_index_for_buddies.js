23c4493ddc9d965e9219d358545233b805790731
Agustin Gabrielli <agabrielli@fi.uba.ar>
2024-10-12 18:55:16 -0300

    Created unique index for connections and fixed geospatial index for elders

--- migrations/20240929190040-geospatial_index_for_buddies.js
+++ migrations/20240929190040-geospatial_index_for_buddies.js
@@ -1,9 +1,9 @@
 module.exports = {
   async up(db, client) {
-    await db.collection('elders').createIndex({ "personalData.address.coordinates": "2dsphere" })
+    await db.collection('buddies').createIndex({ "personalData.address.coordinates": "2dsphere" })
   },
 
   async down(db, client) {
-    await db.collection('elders').dropIndex("personalData.address.coordinates_2dsphere");
+    await db.collection('buddies').dropIndex("personalData.address.coordinates_2dsphere");
   }
 };
